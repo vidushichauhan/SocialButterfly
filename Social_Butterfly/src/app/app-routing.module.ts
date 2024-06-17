c4281c9e6973ffcac945df9be570caa32f73e618
Vidushi Chauhan <vidushic0@gmail.com>
2024-06-16 22:27:33 -0400

    Login and Create Account page done

--- Social_Butterfly/src/app/app-routing.module.ts
+++ Social_Butterfly/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path:'login', component:LoginComponent},
+  {path:'createAccount', component:CreateAccountComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Social_Butterfly/src/app/app.module.ts
+++ Social_Butterfly/src/app/app.module.ts
@@ -4,18 +4,34 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopbarComponent } from './components/topbar/topbar.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatButtonModule} from '@angular/material/button';
+import { LoginComponent } from './pages/login/login.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
+import {ReactiveFormsModule,FormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopbarComponent
+    TopbarComponent,
+    LoginComponent,
+    CreateAccountComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
