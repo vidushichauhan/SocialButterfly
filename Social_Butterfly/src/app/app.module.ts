72cf0d1a299b1c2cd9058874a0598a0e7ad5388b
Vidushi Chauhan <vidushic0@gmail.com>
2024-06-16 20:54:14 -0400

    navbar start

---
+++ Social_Butterfly/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TopbarComponent } from './components/topbar/topbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TopbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
